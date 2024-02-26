d34b22902b82931d965d3b977a59ad8a68594624
Vitor da Luz Santos <142545890+Vitto633@users.noreply.github.com>
2024-02-26 09:42:01 -0300

    Aula02
    RECEBA!

---
+++ SQL_Aula02.sql
@@ -0,0 +1,24 @@
+create database escola;	
+use escola;
+
+create table tbl_instrutor(
+	cod_instrutor numeric(3) not null primary key, 
+	nome_instrutor varchar(30) not null,
+	fone_instrutor varchar(15)
+);
+
+create table tbl_cliente(
+	cpf_clie char(11) primary key,
+	dt_nasc date not null,
+	nm_clie varchar(30) not null,
+	end_clie varchar(50),
+	cep_clie char(8)
+);
+
+create table tbl_produto(
+	ds_prod varchar(50) not null,
+	cd_prod numeric(6) primary key,
+	preco numeric(10,2) not null,
+	qtde numeric (8,2) not null
+);
+
